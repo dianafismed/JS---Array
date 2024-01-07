20a964a0edcf88d9d67ac858b56bcae55b2c9c9b
Diana Avila <45690382+dianafismed@users.noreply.github.com>
2024-01-07 17:11:28 -0300

    up

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -4,6 +4,26 @@ botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
 function filtrarLivros () {
   const elemento = document.getElementById(this.id)
   const categoria = elemento.value
-  let livrosFiltrados = livros.filter(livro => livro.categoria == categoria)
+  let livrosFiltrados = categoria == 'disponivel' ? filtarPorDisponibilidade() : filtarPorCategoria(categoria)
   exibirLivrosNaTela(livrosFiltrados)
+  if(categoria == 'disponivel'){
+    const valor = valorTotal(livrosFiltrados)
+    exibirTotal(valor)
+  }
 }
+
+function filtarPorCategoria(categoria) {
+  return livros.filter(livro => livro.categoria == categoria)
+}
+
+function filtarPorDisponibilidade() {
+  return livros.filter(livro => livro.quantidade > 0)
+}
+
+function exibirTotal (valor) { 
+  total.innerHTML = `
+    <div class="livros__disponiveis">
+      <p>Todos os livros disponíveis por R$ <span id="valor">${valor}</span></p>
+    </div>
+  `
+ }
--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -1,11 +1,14 @@
 const elementoParaInserirLivros = document.getElementById('livros')
+const total = document.getElementById('valor_total_livros_disponiveis')
 
 function exibirLivrosNaTela (listaDeLivros) { 
+  total.innerHTML = ''
   elementoParaInserirLivros.innerHTML = ''
   listaDeLivros.forEach(livro => {
+    let disponibilidade = livro.quantidade>0 ? 'livro__imagens':'livro__imagens indisponivel' // if ternário
     elementoParaInserirLivros.innerHTML += `
       <div class="livro">
-      <img class="livro__imagens" src="${livro.imagem}" alt="${livro.titulo}" />
+      <img class="${disponibilidade}" src="${livro.imagem}" alt="${livro.titulo}" />
       <h2 class="livro__titulo">${livro.titulo}</h2>
       <p class="livro__descricao">${livro.autor}</p>
       <p class="livro__preco" id="preco">R$ ${livro.preco.toFixed(2)}</p>
